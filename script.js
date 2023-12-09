496ade107589f7ae77f7d05c6c380240f89d41e5
spekhy <145577204+spekhy@users.noreply.github.com>
2023-12-09 22:10:44 +0100

    okej, finally sa mi podarilo onclick creatnut divko, musim to len inak arrangnut lebo to creatuje stale pod buttonmi

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
-const bigAddButton = document.getElementById('but-add-note')
-const resetButton = document.getElementById('but-reset')
-const colorChangeButton = document.getElementById('but-color-change')
+const bigAddButton = document.getElementById('but-add-note').addEventListener('click', addNote)
+const resetButton = document.getElementById('but-reset').addEventListener('click', resetNotes)
+const colorChangeButton = document.getElementById('but-color-change').addEventListener('click', colorChange)
 const deleteThisNoteButton = document.getElementById('svg-delete-button')
 const textArea = document.getElementById('note')
+const mainApp = document.getElementById('app')
 
 function addNote() {
-    
+    let newNote = document.createElement('div')
+    newNote.id = 'first-note'
+
+    mainApp.appendChild(newNote)
 }
