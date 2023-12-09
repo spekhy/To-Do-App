b9fce44052bccc897647bbaff6adf8069059b551
spekhy <145577204+spekhy@users.noreply.github.com>
2023-12-09 18:59:25 +0100

    skusal som aby to vytvorilo note ale akosi to nejde

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 const bigAddButton = document.getElementById('but-add-note')
 const resetButton = document.getElementById('but-reset')
 const colorChangeButton = document.getElementById('but-color-change')
+const deleteThisNoteButton = document.getElementById('svg-delete-button')
+const textArea = document.getElementById('note')
+
+note.appendChild(textArea)
 
 function noteReset() {
 
 }
 
 function addNote() {
-    
+    const note = document.createElement('first-note')
 }
 
 function colorChange() {
